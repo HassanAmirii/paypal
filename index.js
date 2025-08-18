7f4235752f6babe93bd50c5e84b992e7a3e85dcd
Hassan Amiri <hassanamiri.ai@gmail.com>
2025-08-18 11:52:10 +0100

    connected mongoose to set up a db

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ require("dotenv").config;
 const express = require("express");
 const app = express();
 const port = 3000;
-
 const API_KEY = process.env.API_KEY;
 
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://localhost:27017/expense-tracker")
+  .then(() => console.log("Database connected successfully!"))
+  .catch((err) => console.error("Database connection error:", err));
 app.get("/", (req, res) => {
   res.send("hello world");
 });
