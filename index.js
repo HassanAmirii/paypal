b7be81cfc4dd6d1b5631c45c68396c1df70e498c
Hassan Amiri <hassanamiri.ai@gmail.com>
2025-08-26 16:19:47 +0100

    feat: implement user registration and login endpoints with JWT authentication

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose");
 const Expense = require("./models/expense");
 const User = require("./models/user");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const express = require("express");
+require("dotenv").config();
 const app = express();
 app.use(express.json());
 const port = 3000;
@@ -10,6 +13,56 @@ mongoose
   .connect("mongodb://localhost:27017/expense-tracker")
   .then(() => console.log("Database connected successfully!"))
   .catch((err) => console.error("Database connection error:", err));
+
+app.post("/register", async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    const newUser = new User({
+      username,
+      email,
+      password,
+    });
+    await newUser.save();
+    res
+      .status(201)
+      .json({ message: "succesfully created your account", account: newUser });
+  } catch (error) {
+    if (error.code === 11000) {
+      return res.status(409).json({ error: "This email is already in use" });
+    }
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(401).json({ error: "invalid credentials" });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({ error: "invalid credentials" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.get("/users", async (req, res) => {
+  try {
+    const users = await User.find();
+
+    res.status(200).json({
+      message: "successfully retrieved all expenses",
+      users: users,
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
 app.post("/expenses", async (req, res) => {
   try {
@@ -102,5 +155,5 @@ app.patch("/expenses/:id", async (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`paypal listening on localhost:${port}/expenses`);
+  console.log(`app is listening on localhost:${port}/`);
 });
